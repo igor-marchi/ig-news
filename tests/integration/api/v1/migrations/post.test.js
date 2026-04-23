5ed2da2ebb5ef2d5b7a0e8821e1f44c429aa40c7
Igor Marchi <igoraugustomarchi@gmail.com>
2026-04-22 23:16:49 -0300

    feat: create `orchestrator.js` and `async-retry`

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,10 +1,10 @@
 import database from "infra/database";
+import orchestrator from "tests/orchestrator";
 
-async function cleanDatabase() {
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
   await database.query("drop schema public cascade; create schema public;");
-}
-
-beforeAll(cleanDatabase);
+});
 
 test("POST to /api/v1/migrations should return status OK", async () => {
   const url = "http://localhost:3000/api/v1/migrations";
